aed45b6d1877d9a10d0e33aae3c3da4c24318a41
GitHub Action <action@github.com>
2023-08-28 11:51:23 +0000

    Replaced old code with new code from API

--- sudokutools.py
+++ sudokutools.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+File: sudokutools.py
+
+Updated Code:
 
 from random import randint, shuffle
-
+from multiprocessing import Pool
 
 def print_board(board):
     """
@@ -103,6 +104,38 @@ def solve(board):
     return False
 
 
+def fill_cells(board, row, col):
+    """
+    Fills the remaining cells of the sudoku board with backtracking.
+
+    Args:
+        board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
+        row (int): The current row index to fill.
+        col (int): The current column index to fill.
+
+    Returns:
+        bool: True if the remaining cells are successfully filled, False otherwise.
+    """
+
+    if row == 9:
+        return True
+    if col == 9:
+        return fill_cells(board, row + 1, 0)
+
+    if board[row][col] != 0:
+        return fill_cells(board, row, col + 1)
+
+    for num in range(1, 10):
+        if valid(board, (row, col), num):
+            board[row][col] = num
+
+            if fill_cells(board, row, col + 1):
+                return True
+
+    board[row][col] = 0
+    return False
+
+
 def generate_board():
     """
     Generates a random sudoku board with fewer initial numbers.
@@ -122,37 +155,6 @@ def generate_board():
                 board[i + row][i + col] = nums.pop()
 
     # Fill the remaining cells with backtracking
-    def fill_cells(board, row, col):
-        """
-        Fills the remaining cells of the sudoku board with backtracking.
-
-        Args:
-            board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
-            row (int): The current row index to fill.
-            col (int): The current column index to fill.
-
-        Returns:
-            bool: True if the remaining cells are successfully filled, False otherwise.
-        """
-
-        if row == 9:
-            return True
-        if col == 9:
-            return fill_cells(board, row + 1, 0)
-
-        if board[row][col] != 0:
-            return fill_cells(board, row, col + 1)
-
-        for num in range(1, 10):
-            if valid(board, (row, col), num):
-                board[row][col] = num
-
-                if fill_cells(board, row, col + 1):
-                    return True
-
-        board[row][col] = 0
-        return False
-
     fill_cells(board, 0, 0)
 
     # Remove a greater number of cells to create a puzzle with fewer initial numbers
@@ -168,3 +170,8 @@ if __name__ == "__main__":
     print_board(board)
     solve(board)
     print_board(board)
+
+
+Explanation:
+No explanation
+
