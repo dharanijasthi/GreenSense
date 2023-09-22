d401999ff346199043cb04d7f852e9d96ef4cda5
dharanijasthi <dharanijasthi12@gmail.com>
2023-09-22 17:47:15 +0530

    changed the structure and modified files

--- test.py
+++ test.py
@@ -15,3 +15,14 @@ def give_output(n):
         return n
     else:
         return give_output(n-1) + give_output(n-2)
+    
+def subArraySum(arr, n, sum):
+    for i in range(0,n):
+        currentSum = arr[i]
+        if(currentSum == sum):
+            return
+        else:
+            for j in range(i+1,n):
+                currentSum += arr[j]
+                if(currentSum == sum):
+                    return
