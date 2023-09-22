dd778101938d8086cb629308c5427b13586cd3c5
dharanijasthi <dharanijasthi12@gmail.com>
2023-09-22 15:09:04 +0530

    updated code

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import math
-
 def create_large_list():
-    large_list = list(range(1000000))
+    large_list = []
+    for i in range(1000000):
+        large_list.append(i)
     return large_list
 
 def factorial(n):
-    return math.factorial(n)
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 def give_output(n):
     if n <= 1:
