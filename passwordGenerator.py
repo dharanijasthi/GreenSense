3ffd3193eb92fc7d946ff61ea061368c3c4c1ae1
dharanijasthi <dharanijasthi12@gmail.com>
2023-09-25 12:33:09 +0530

    updated content

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,30 +1,14 @@
-def coinChange(coins, amount):
-    def recursiveChange(remaining):
-        # base case: if remaining amount is negative, return -1
-        if remaining < 0:
-            return -1
-        # base case: if remaining amount is 0, return 0 (no more coins needed)
-        if remaining == 0:
-            return 0
-        # initialize minimum to a large value
-        minimum = float('inf')
-        for coin in coins:
-            result = recursiveChange(remaining - coin)
-            # if result is not -1 and is smaller than current minimum, update minimum
-            if result >= 0 and result < minimum:
-                minimum = result + 1
-        # if minimum is still inf, return -1, else return minimum
-        return -1 if minimum == float('inf') else minimum
+def climbStairs_brute(n):
+    if n <= 1:
+        return 1
+    return climbStairs_brute(n - 1) + climbStairs_brute(n - 2)
 
-    return recursiveChange(amount)
 
-def longestCommonSubsequence(text1, text2):
-    def lcs(i, j):
-        if i < 0 or j < 0:
-            return 0
-        if text1[i] == text2[j]:
-            return 1 + lcs(i - 1, j - 1)
-        else:
-            return max(lcs(i - 1, j), lcs(i, j - 1))
 
-    return lcs(len(text1) - 1, len(text2) - 1)
+def longestCommonSubsequence_brute(text1, text2):
+    if not text1 or not text2:
+        return 0
+    if text1[-1] == text2[-1]:
+        return 1 + longestCommonSubsequence_brute(text1[:-1], text2[:-1])
+    else:
+        return max(longestCommonSubsequence_brute(text1[:-1], text2), longestCommonSubsequence_brute(text1, text2[:-1]))
