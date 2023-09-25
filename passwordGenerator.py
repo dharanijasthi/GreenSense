97211201286a7e3199f4ad8444c6d7b614e42d88
dharanijasthi <dharanijasthi12@gmail.com>
2023-09-25 12:23:55 +0530

    updated code

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,37 +1,30 @@
-"""Snake, classic arcade game.
+def coinChange(coins, amount):
+    def recursiveChange(remaining):
+        # base case: if remaining amount is negative, return -1
+        if remaining < 0:
+            return -1
+        # base case: if remaining amount is 0, return 0 (no more coins needed)
+        if remaining == 0:
+            return 0
+        # initialize minimum to a large value
+        minimum = float('inf')
+        for coin in coins:
+            result = recursiveChange(remaining - coin)
+            # if result is not -1 and is smaller than current minimum, update minimum
+            if result >= 0 and result < minimum:
+                minimum = result + 1
+        # if minimum is still inf, return -1, else return minimum
+        return -1 if minimum == float('inf') else minimum
 
-Excercises
+    return recursiveChange(amount)
 
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to arrow keys.
+def longestCommonSubsequence(text1, text2):
+    def lcs(i, j):
+        if i < 0 or j < 0:
+            return 0
+        if text1[i] == text2[j]:
+            return 1 + lcs(i - 1, j - 1)
+        else:
+            return max(lcs(i - 1, j), lcs(i, j - 1))
 
-"""
-
-from turtle import *
-from random import randrange
-from freegames import square, vector
-
-food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
-
-def change(x, y):
-    "Change snake direction."
-    aim.x = x
-    aim.y = y
-
-def inside(head):
-    "Return True if head inside boundaries."
-    return -200 < head.x < 190 and -200 < head.y < 190
-
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
-done()
+    return lcs(len(text1) - 1, len(text2) - 1)
