2e15e1aaf4cc0027cca86d91b4e5366998db90ec
GitHub Action <action@github.com>
2023-09-25 06:55:24 +0000

    Replaced old code with new code from API

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,30 +1,37 @@
+
+# Optimized coinChange function using dynamic programming
 def coinChange(coins, amount):
-    def recursiveChange(remaining):
-        # base case: if remaining amount is negative, return -1
-        if remaining < 0:
-            return -1
-        # base case: if remaining amount is 0, return 0 (no more coins needed)
-        if remaining == 0:
-            return 0
-        # initialize minimum to a large value
-        minimum = float('inf')
+    # Create a list to store the minimum number of coins needed for each amount
+    dp = [float('inf')] * (amount + 1)
+    # Base case: 0 coins needed for amount 0
+    dp[0] = 0
+    # Iterate through each amount from 1 to the target amount
+    for i in range(1, amount + 1):
+        # Iterate through each coin
         for coin in coins:
-            result = recursiveChange(remaining - coin)
-            # if result is not -1 and is smaller than current minimum, update minimum
-            if result >= 0 and result < minimum:
-                minimum = result + 1
-        # if minimum is still inf, return -1, else return minimum
-        return -1 if minimum == float('inf') else minimum
+            # If the coin value is less than or equal to the current amount
+            if coin <= i:
+                # Update the minimum number of coins needed for the current amount
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    # If the minimum number of coins needed for the target amount is still infinity, return -1
+    if dp[amount] == float('inf'):
+        return -1
+    # Otherwise, return the minimum number of coins needed
+    return dp[amount]
 
-    return recursiveChange(amount)
-
+# Optimized longestCommonSubsequence function using dynamic programming
 def longestCommonSubsequence(text1, text2):
-    def lcs(i, j):
-        if i < 0 or j < 0:
-            return 0
-        if text1[i] == text2[j]:
-            return 1 + lcs(i - 1, j - 1)
-        else:
-            return max(lcs(i - 1, j), lcs(i, j - 1))
-
-    return lcs(len(text1) - 1, len(text2) - 1)
+    # Create a 2D matrix to store the lengths of the longest common subsequences
+    dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+    # Iterate through each character in text1
+    for i in range(1, len(text1) + 1):
+        # Iterate through each character in text2
+        for j in range(1, len(text2) + 1):
+            # If the characters are equal, add 1 to the length of the longest common subsequence
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            # Otherwise, take the maximum length from the previous row or column
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    # Return the length of the longest common subsequence
+    return dp[len(text1)][len(text2)]
