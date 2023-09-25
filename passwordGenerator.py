19a03478ff0493fa179d4a64d95d938d8fccbd29
GitHub Action <action@github.com>
2023-09-25 07:04:26 +0000

    Replaced old code with new code from API

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,14 +1,23 @@
-def climbStairs_brute(n):
+
+def climbStairs_optimized(n):
     if n <= 1:
         return 1
-    return climbStairs_brute(n - 1) + climbStairs_brute(n - 2)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
 
 
-
-def longestCommonSubsequence_brute(text1, text2):
-    if not text1 or not text2:
-        return 0
-    if text1[-1] == text2[-1]:
-        return 1 + longestCommonSubsequence_brute(text1[:-1], text2[:-1])
-    else:
-        return max(longestCommonSubsequence_brute(text1[:-1], text2), longestCommonSubsequence_brute(text1, text2[:-1]))
+def longestCommonSubsequence_optimized(text1, text2):
+    m = len(text1)
+    n = len(text2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
